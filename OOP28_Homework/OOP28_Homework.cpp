1e1ca6f7cd18ae91678fcb81531f147e12c68ecc
Vyuga Snow <Wolf-snow00@mail.ru>
2023-02-20 19:33:29 +0300

    Добавьте файлы проекта.

---
+++ OOP28_Homework/OOP28_Homework.cpp
@@ -0,0 +1,35 @@
+﻿
+
+#include <iostream>
+#include "Vocabular.h"
+
+using namespace std;
+
+// Initial text:
+// 
+//He rushed to the corner, put his hand under the wallpaper and began to pull out 
+//things and load his pockets with them. There were eight pieces in all: 
+//two small boxes of earrings or something like that - he didn't 
+//take a good look; then four small morocco cases. 
+//One chain was simply wrapped in newsprint. 
+//Something else in newsprint, it seems an order ...
+
+
+int main()
+{
+    Vocabular a;
+    a.readFromFile("C:\\Test\\test.txt");
+    a.dataOfAllWords();
+    //a.print();
+    //cout << "\n\n";
+    //a.printMostRepeats();
+    a.recordingToFile("C:\\Test\\test2.txt");
+
+
+
+
+
+
+
+
+}
---
+++ OOP28_Homework/Vocabular.h
@@ -0,0 +1,134 @@
+#pragma once
+#include <iostream>
+#include <map>
+#include <string>
+#include <cctype>
+#include <algorithm>
+
+using namespace std;
+
+
+class Vocabular {
+	string mainStr;
+	map<string, int> words;
+	string mostRepeatWord;
+
+	int findRepeatWord(string tempStr,map<string,int> tempMap) {
+		map<string, int>::iterator iter = tempMap.begin(); // Temp iterator
+
+		for (; iter != tempMap.end(); iter++) {
+			if (tempStr == iter->first) // If this word (tempStr) already exists
+				return iter->second;
+		}
+		// If this map dont' has this word (tempStr)
+		return 0;
+	}
+
+public:
+
+	void readFromFile(const char* path);
+
+	void dataOfAllWords();
+
+	void print();
+
+	void printMostRepeats();
+
+	void recordingToFile(const char* path);
+
+	string getMainStr();
+
+
+};
+
+
+void Vocabular::readFromFile(const char* path) {
+	FILE* out;
+	char temp;
+
+	if (!fopen_s(&out, path, "r")) {
+		while (!feof(out)) {
+			temp = fgetc(out);
+			mainStr.push_back(temp);
+		}
+		fclose(out);
+	}
+	// Entire string in lowercase
+	transform(mainStr.begin(), mainStr.end(), mainStr.begin(), tolower);
+}
+
+void Vocabular::dataOfAllWords() {
+	string tempStr;
+	int max_repeats = 0;
+	int counter_word = 0;
+
+	for (auto i = mainStr.begin(); i < mainStr.end(); i++) {
+		if (*i != ' ' && *i != '.' && *i != '!' &&
+			*i != '?' && *i != ':' && *i != ',' &&
+			*i != ';' && *i != '\n' && *i != '-')
+			tempStr.push_back(*i);
+
+		else {
+			// Find repeat words
+			counter_word = findRepeatWord(tempStr, words) + 1;
+			// Add new pair or replace old
+			words[tempStr] = counter_word;
+
+			// Find maximum repeats of word
+			if (max_repeats < counter_word) {
+				max_repeats = counter_word;
+				mostRepeatWord = tempStr;
+			}
+			// Clear temp variables 
+			tempStr.clear();
+		}
+	}
+
+}
+
+void Vocabular::print() {
+	map<string, int>::iterator iter = words.begin();
+	if (iter != words.end()) {
+		for (; iter != words.end(); iter++) {
+			cout << iter->first << " - " << iter->second << endl;
+		}
+	}
+	else
+		cout << "Vocabular is empty\n";
+}
+
+void Vocabular::printMostRepeats() {
+	map<string, int>::iterator iter = words.begin();
+	// I don't know why this "  " is exists in my map... 
+	if (iter != words.end()) {
+		iter = words.find(mostRepeatWord);
+		cout << "Most repeat word:\n";
+		cout << iter->first << " - " << iter->second << endl;
+	}
+	else
+		cout << "Vocabular is empty\n";
+}
+
+void Vocabular::recordingToFile(const char* path) {
+	FILE* in;
+
+	char* buffer = new char[125];
+	if (!fopen_s(&in, path, "w")) {
+		map<string, int>::iterator iter = words.begin();
+		for (; iter != words.end(); iter++) {
+			strcpy_s(buffer, 126, iter->first.c_str());
+			fputs(buffer, in);
+			fputc(' ', in);
+			fputc('-', in);
+			fputc(' ', in);
+			fprintf(in,"%d", iter->second);
+			fputc('\n', in);
+		}
+	}
+
+}
+
+string Vocabular::getMainStr() {
+	return mainStr;
+}
+
